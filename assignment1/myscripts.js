9edd9f4f7773c155a0f85e2561b44ee101262779
malin888 <mabim149@newschool.edu>
2021-09-16 10:07:21 -0400

    ggg

---
+++ assignment1/myscripts.js
@@ -0,0 +1,7 @@
+var counter = 0
+
+// function to increase the counter variable by 1 on each click:
+function count() {
+    counter = counter + 1
+    document.getElementById("counter").innerHTML = counter
+}
