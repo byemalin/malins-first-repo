120d96651c72b820e628794ec28ed5e6b8643b6d
malin888 <mabim149@newschool.edu>
2021-11-04 13:23:13 -0400

    Updating CAS proto progress

--- CAS_Prototype_1/Scripts/script.js
+++ CAS_Prototype_1/Scripts/script.js
@@ -2,11 +2,12 @@
 window.addEventListener('DOMContentLoaded', async () => {
     console.log('DOM fully loaded and parsed');
 
+    console.log("look")
+
 //Experimenting with making hover change color
 
 let zambia = document.querySelector('#ZM')
 
-console.log(zambia)
 zambia.addEventListener("mouseover", respondMouseOver);
 zambia.addEventListener("mouseout", respondMouseOut);
 zambia.addEventListener("click", respondClick);
@@ -25,18 +26,30 @@ let GDPData= await d3.csv("Assets/data/PopulationandGDP.csv", (d)=>{
     return d
 })
 
+let ForbesByName = await d3.csv("Assets/data/ForbesByName.csv", (d)=>{
+    return d
+})
+
+
+console.log('Forbes', ForbesByName)
+
+
+
+//Creating the list of billionaires:
+async function getpeople(
+people = ForbesbyName.map((d) => {
+    return d[0];
+  })
+)
+
+
+
 
 //Filtering to get data of one country (Zambia)
 
 DataForZambia = GDPData.filter(
     (d) => d.Country == "Zambia"
-  )
-  console.log(DataForZambia)
-
-  console.log(DataForZambia[0]["TotalGDP"])
-
-});
-
+)
 //Making the size of circle = Zambia total GDP
 
 
@@ -44,3 +57,35 @@ function respondClick(event) {
     // document.documentElement.style.setProperty("--c-size", DataForZambia[0]["TotalGDP"])
     document.documentElement.style.setProperty("--c-size", '300px')
 }
+
+
+
+// Adding heatmap 
+
+// colorscale= d3.scaleLog().domain([0, 28643892530.221188, 23266768927425.203]).range(["black", "grey", "red"])
+
+
+// GDPData.map(d => {
+
+//     CountryCode = d.Code
+
+//     var ShortCode = CountryCode.substring(0, 2); 
+
+    
+
+
+//     let countrySVG = document.querySelector('#' + ShortCode)
+
+
+//     if(countrySVG){
+//         countrySVG.style.fill = colorscale(d.TotalGDP)
+//     }else{
+//         console.log('Hi')
+//     }
+// })
+
+
+
+
+
+}); //Close DOM
